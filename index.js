fd47398240d6237a0ccc1711c14def49b00c0eb4
Evgeny <zubarev.evgeny@gmail.com>
2017-04-08 15:58:45 +0300

    Trends inline search

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Telegraf = require('telegraf')
 const msg = require('./messages')
 
 const bot = require('./service/bot-engine')
+const inlineSearch = require('./service/inline-search')
 
 const app = new Telegraf('375464631:AAFxcfWfIamyDI9zlOI1ZHqsYNkVeUsCxrM')
 
@@ -28,25 +29,7 @@ app.command('bantrend', (ctx) => {
 app.on('inline_query', (ctx) => {
     const query = ctx.inlineQuery.query || ''
 
-    const tweets = [
-        { id: '111111', text: 'aaa', imageUrl: 'https://pbs.twimg.com/media/C84YcrdXkAAmPYS.jpg', url: 'https://twitter.com/eurogamer/status/850657472994058241' },
-        { id: '222222', text: 'vvv', imageUrl: 'https://pbs.twimg.com/media/C84YcrdXkAAmPYS.jpg', url: 'https://twitter.com/eurogamer/status/850657472994058241' }
-    ]
-
-    const results = tweets
-        //.filter((country) => country.name.toLowerCase().indexOf(query.toLowerCase()) !== -1)
-        .map((tweet) => {
-            return {
-                id: tweet.id,
-                title: tweet.text,
-                type: 'article',
-                thumb_url: tweet.imageUrl,
-                input_message_content: {
-                    message_text: `${tweet.text} code: *${tweet.url}*`,
-                    parse_mode: 'Markdown'
-                }
-            }
-        })
+    var results = inlineSearch.search(query);
     return ctx.answerInlineQuery(results)
 })
 
