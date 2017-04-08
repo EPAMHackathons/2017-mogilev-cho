e6e7530fd8b3a03f27b5d7c2da2751b242aa72a6
Evgeny <zubarev.evgeny@gmail.com>
2017-04-08 15:44:09 +0300

    enable inline mode

--- index.js
+++ index.js
@@ -16,14 +16,39 @@ app.command('search', (ctx) => {
     bot.search(ctx.message.text)
 })
 
-app.command('stop-trend', (ctx) => {
-    console.log('stop-trend', ctx.from)
+app.command('bantrend', (ctx) => {
+    console.log('bantrend', ctx.from)
     bot.stopTrend(ctx.message.text)
 })
 
-app.hears('hi', (ctx) => ctx.reply('Hey there!'))
+//app.hears('hi', (ctx) => ctx.reply('Hey there!'))
 
-app.on('sticker', (ctx) => ctx.reply('👍'))
+//app.on('sticker', (ctx) => ctx.reply('👍'))
+
+app.on('inline_query', (ctx) => {
+    const query = ctx.inlineQuery.query || ''
+
+    const tweets = [
+        { id: '111111', text: 'aaa', imageUrl: 'https://pbs.twimg.com/media/C84YcrdXkAAmPYS.jpg', url: 'https://twitter.com/eurogamer/status/850657472994058241' },
+        { id: '222222', text: 'vvv', imageUrl: 'https://pbs.twimg.com/media/C84YcrdXkAAmPYS.jpg', url: 'https://twitter.com/eurogamer/status/850657472994058241' }
+    ]
+
+    const results = tweets
+        //.filter((country) => country.name.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+        .map((tweet) => {
+            return {
+                id: tweet.id,
+                title: tweet.text,
+                type: 'article',
+                thumb_url: tweet.imageUrl,
+                input_message_content: {
+                    message_text: `${tweet.text} code: *${tweet.url}*`,
+                    parse_mode: 'Markdown'
+                }
+            }
+        })
+    return ctx.answerInlineQuery(results)
+})
 
 app.on('message', (ctx) => {
     var resp = bot.analyze(ctx.message.text)
